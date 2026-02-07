bade1a90a3f0604f2972be36c253853c6aac7496
Mayank Sharma <unichain0@gmail.com>
2026-02-07 10:33:30 +0530

    committed

---
+++ linkedlist.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *curr = head;
+        ListNode *prev = nullptr;
+        while(curr != nullptr) {
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
+int main() {
+    // Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution solution;
+    ListNode* reversedHead = solution.reverseList(head);
+
+    // Print the reversed linked list
+    ListNode* current = reversedHead;
+    while (current != nullptr) {
+        cout << current->val << " ";
+        current = current->next;
+    }
+    cout << endl;
+
+    return 0;
+}
